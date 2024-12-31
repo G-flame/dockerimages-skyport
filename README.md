3de879b2c66cbce6acc60714cbba374bc432c349
g-flame <thegreenflame1507@gmail.com>
2024-12-31 15:42:35 +0530

    cleaned up and add a bedrock server installer
    cleaned up and add a bedrock server installer

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# fabric-skyport-image
-fabric minecraft dockerized for skyport panel
-
-how to use ?
-download the .json file you want and uplpad it to the images tab in skyport panel
+## DOCKER FILES
--- buildit.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-git clone --recurse-submodules https://github.com/JSPrismarine/JSPrismarine.git
-mv JSPrismarine/* /app/data
-wget -qO- https://get.pnpm.io/install.sh | ENV="$HOME/.bashrc" SHELL="$(which bash)" bash -
-pnpm run install
-pnpm run build
-echo -e "install done !"
